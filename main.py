29c06238e733feca1788a75aa570b8e523c0152f
1996LM <lmiguel.hoyos@gmail.com>
2022-08-24 21:02:02 -0500

    commit inicial

---
+++ main.py
@@ -0,0 +1,7 @@
+#venv es el nombre para la libreria virtual enviroment   python -m venv nombre - donde nombre usualmente es venv
+#entorno virtual es un lugar donde colocar las librerias especificas
+#  con sus versiones especificas que voy a utilizar en el proyecto
+#tambien para aislar el proyecto
+#ahora vamos activar el entorno virtual
+import numpy as np
+arreglo = np.array([1,2,3])
